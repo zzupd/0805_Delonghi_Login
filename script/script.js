272afd7ca8b1d40ae581241b4ccbb60b7b72a9b1
zzupd <zzupd@naver.com>
2021-08-06 15:45:16 +0900

    0806_GNB작업중(서브&배경디버깅중)

--- script/script.js
+++ script/script.js
@@ -1,6 +1,12 @@
 $(function(){
 
-
-
+    $(".mainLi").mouseover(function(){
+        $(this).children("ul").stop().show();
+        $("#subMenuBG").stop().show();
+    });
+    $(".mainLi").mouseout(function(){
+        $(this).children("ul").stop().hide();
+        $("#subMenuBG").stop().hide();
+    });
 
 });
